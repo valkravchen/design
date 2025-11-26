df307cc7ae4958bc73e3e2cf96ce36b22eac4705
Valk <kravchenkoval@mail.ru>
2025-11-26 16:32:23 +0300

    Add Configuration

--- src/main/java/ru/App.java
+++ src/main/java/ru/App.java
@@ -1,13 +1,7 @@
 package ru;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
+public class App {
+    public static void main( String[] args ) {
         System.out.println( "Hello World!" );
     }
 }
