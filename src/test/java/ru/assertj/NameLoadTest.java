880b9c1fe5016fdc2554d7cf1a371aa13d237f90
valkravchen <100293539+valkravchen@users.noreply.github.com>
2025-11-27 19:39:49 +0300

    26.4 Утверждения с исключениями

--- src/test/java/ru/assertj/NameLoadTest.java
+++ src/test/java/ru/assertj/NameLoadTest.java
@@ -1,15 +1,53 @@
 package ru.assertj;
 
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 
 import static org.assertj.core.api.Assertions.*;
 
 class NameLoadTest {
+
+    private NameLoad nameLoad;
+
+    @BeforeEach
+    void setUp() {
+        nameLoad = new NameLoad();
+    }
+
     @Test
     void whenParseEmptyArray() {
-        NameLoad nameLoad = new NameLoad();
         assertThatThrownBy(nameLoad::parse)
                 .isInstanceOf(IllegalArgumentException.class)
-                .hasMessageContaining("Names array is empty");
+                .hasMessage("Names array is empty");
+    }
+
+    @Test
+    void whenParseWithoutEquals() {
+        String result = "key:value";
+        assertThatThrownBy(() -> nameLoad.parse(result))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("this name: %s does not contain the symbol '='", result);
+    }
+
+    @Test
+    void whenParseWithoutKey() {
+        String result = "=value";
+        assertThatThrownBy(() -> nameLoad.parse(result))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("this name: %s does not contain a key", result);
+    }
+
+    @Test
+    void whenParseWithoutValue() {
+        String result = "key=";
+        assertThatThrownBy(() -> nameLoad.parse(result))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("this name: %s does not contain a value", result);
+    }
+
+    @Test
+    void whenGetMapWithoutParse() {
+        assertThatThrownBy(nameLoad::getMap)
+                .isInstanceOf(IllegalStateException.class)
+                .hasMessage("collection contains no data");
     }
 }
