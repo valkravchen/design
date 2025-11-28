293d79ae9d4ac244a9183102d217f6ff43a51cf4
valkravchen <100293539+valkravchen@users.noreply.github.com>
2025-11-28 20:44:04 +0300

    26.5 Утверждения с коллекциями

--- src/test/java/ru/iterator/BackwardArrayIteratorTest.java
+++ src/test/java/ru/iterator/BackwardArrayIteratorTest.java
@@ -1,14 +1,16 @@
 package ru.iterator;
 
 import static org.assertj.core.api.Assertions.*;
+
 import org.junit.jupiter.api.Test;
+
 import java.util.NoSuchElementException;
 
 class BackwardArrayIteratorTest {
     @Test
     void whenMultiCallHasNextThenTrue() {
         BackwardArrayIterator iterator = new BackwardArrayIterator(
-                new int[] {1, 2, 3, 4}
+                new int[]{1, 2, 3, 4}
         );
         assertThat(iterator.hasNext()).isTrue();
         assertThat(iterator.hasNext()).isTrue();
@@ -20,20 +22,17 @@ class BackwardArrayIteratorTest {
     @Test
     void whenMultiCallHasNextThenNext() {
         BackwardArrayIterator iterator = new BackwardArrayIterator(
-                new int[] {1, 2, 3}
+                new int[]{1, 2, 3}
         );
         assertThat(iterator.hasNext()).isTrue();
         assertThat(iterator.hasNext()).isTrue();
         assertThat(iterator.hasNext()).isTrue();
         assertThat(iterator.next()).isEqualTo(3);
-
     }
 
     @Test
     void whenReadSequence() {
-        BackwardArrayIterator iterator = new BackwardArrayIterator(
-                new int[] {1, 2, 3}
-        );
+        BackwardArrayIterator iterator = new BackwardArrayIterator(new int[]{1, 2, 3});
         assertThat(iterator.next()).isEqualTo(3);
         assertThat(iterator.next()).isEqualTo(2);
         assertThat(iterator.next()).isEqualTo(1);
@@ -43,7 +42,7 @@ class BackwardArrayIteratorTest {
     @Test
     void whenNextFromEmpty() {
         BackwardArrayIterator iterator = new BackwardArrayIterator(
-                new int[] {}
+                new int[]{}
         );
         assertThatThrownBy(iterator::next)
                 .isInstanceOf(NoSuchElementException.class);
