e33a5b7797459397a1d39e045b4c52d59a240a99
valkravchen <100293539+valkravchen@users.noreply.github.com>
2025-11-30 19:03:21 +0300

    27.3 Итератор чётных чисел

---
+++ src/main/java/ru/iterator/EvenNumbersIterator.java
@@ -0,0 +1,32 @@
+package ru.iterator;
+
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+
+public class EvenNumbersIterator implements Iterator<Integer> {
+    private int[] data;
+    private int index = 0;
+
+    public EvenNumbersIterator(int[] data) {
+        this.data = data;
+    }
+
+    @Override
+    public boolean hasNext() {
+        while (index < data.length) {
+            if (data[index] % 2 == 0) {
+                break;
+            }
+            index++;
+        }
+        return index < data.length;
+    }
+
+    @Override
+    public Integer next() {
+        if (!hasNext()) {
+            throw new NoSuchElementException();
+        }
+        return data[index++];
+    }
+}
