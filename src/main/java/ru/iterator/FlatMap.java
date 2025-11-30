dad02d804622bfb18766da88b77547075270aead
valkravchen <100293539+valkravchen@users.noreply.github.com>
2025-11-30 07:14:16 +0300

    27.4 FlatMap для Iterator

---
+++ src/main/java/ru/iterator/FlatMap.java
@@ -0,0 +1,29 @@
+package ru.iterator;
+
+import java.util.*;
+
+public class FlatMap<T> implements Iterator<T> {
+    private final Iterator<Iterator<T>> data;
+    private Iterator<T> cursor = Collections.emptyIterator();
+
+    public FlatMap(Iterator<Iterator<T>> data) {
+        this.data = data;
+    }
+
+    @Override
+    public boolean hasNext() {
+        while (!cursor.hasNext() && data.hasNext()) {
+            cursor = data.next();
+        }
+        return cursor.hasNext();
+    }
+
+    @Override
+    public T next() {
+        if (!hasNext()) {
+            throw new NoSuchElementException();
+        }
+
+        return cursor.next();
+    }
+}
