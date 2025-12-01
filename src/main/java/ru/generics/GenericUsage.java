1673d4974760719ee63850e71424521c5029f000
Valk <kravchenkoval@mail.ru>
2025-12-01 09:27:20 +0300

    28.1 Обобщённые типы generics

--- src/main/java/ru/generics/GenericUsage.java
+++ src/main/java/ru/generics/GenericUsage.java
@@ -4,11 +4,13 @@ import java.util.*;
 
 public class GenericUsage {
     public static void main(String[] args) {
-        List<String> list = new ArrayList<>();
+        List list = new ArrayList<>();
         list.add("first");
         list.add("second");
         list.add("third");
-        for (String line : list) {
+        String s = (String) list.get(1);
+        for (int i = 0; i < list.size(); i++) {
+            String line = (String) list.get(i);
             System.out.println("Текущий элемент: " + line);
         }
     }
