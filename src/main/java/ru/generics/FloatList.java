6d67d6391d481bddadb931fdb6c4de215da33a55
Valk <kravchenkoval@mail.ru>
2025-12-01 12:14:20 +0300

    28.1 Обобщённые типы generics

---
+++ src/main/java/ru/generics/FloatList.java
@@ -0,0 +1,14 @@
+package ru.generics;
+
+import java.lang.reflect.ParameterizedType;
+import java.util.ArrayList;
+
+public class FloatList extends ArrayList<Float> {
+    public static void main(String[] args) {
+        ArrayList<Float> listOfNumbers = new FloatList();
+        Class actual = listOfNumbers.getClass();
+        ParameterizedType type = (ParameterizedType) actual.getGenericSuperclass();
+        Class parameter = (Class) type.getActualTypeArguments()[0];
+        System.out.println(parameter);
+    }
+}
