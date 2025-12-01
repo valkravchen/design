9f04e5ad8748c99a4cad057561a679868ed0a163
Valk <kravchenkoval@mail.ru>
2025-12-01 11:02:38 +0300

    28.1 Обобщённые типы generics

--- src/main/java/ru/generics/GenericsClass.java
+++ src/main/java/ru/generics/GenericsClass.java
@@ -1,4 +1,26 @@
 package ru.generics;
 
-public class GenericsClass {
+public class GenericsClass<K, V> {
+    private K key;
+    private V value;
+
+    public GenericsClass(K key, V value) {
+        this.key = key;
+        this.value = value;
+    }
+
+    @Override
+    public String toString() {
+        return "GenericsClass{" +
+                "key=" + key +
+                ", value=" + value +
+                '}';
+    }
+
+    public static void main(String[] args) {
+        GenericsClass<String, String> first = new GenericsClass<>("First key", "First value");
+        System.out.println("Вывод в консоль: " + first);
+        GenericsClass<Integer, String> second = new GenericsClass<>(12345, "Second value");
+        System.out.println("Вывод в консоль: " + second);
+    }
 }
