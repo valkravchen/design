73361f945fce6d44adba1056c0a43bf9d488566f
Valk <kravchenkoval@mail.ru>
2025-12-04 15:33:30 +0300

    28.2 Реализовать interface Store<T extends Base>

--- src/main/java/ru/generics/MemStore.java
+++ src/main/java/ru/generics/MemStore.java
@@ -7,17 +7,21 @@ public final class MemStore<T extends Base> implements Store<T> {
 
     @Override
     public void add(T model) {
-        storage.put(model.getId(), model);
+        storage.putIfAbsent(model.getId(), model);
     }
 
     @Override
     public boolean replace(String id, T model) {
+        if (storage.containsKey(id)) {
+            storage.put(id, model);
+            return true;
+        }
         return false;
     }
 
     @Override
     public void delete(String id) {
-
+        storage.remove(id);
     }
 
     @Override
--- src/main/java/ru/generics/UserStore.java
+++ src/main/java/ru/generics/UserStore.java
@@ -5,21 +5,21 @@ public class UserStore implements Store<User> {
 
     @Override
     public void add(User model) {
-
+        store.add(model);
     }
 
     @Override
     public boolean replace(String id, User model) {
-        return false;
+        return store.replace(id, model);
     }
 
     @Override
     public void delete(String id) {
-
+        store.delete(id);
     }
 
     @Override
     public User findById(String id) {
-        return null;
+        return store.findById(id);
     }
 }
