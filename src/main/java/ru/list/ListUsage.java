c5b1668668fea9e6f7d7a48638cde03701c55611
valkravchen <100293539+valkravchen@users.noreply.github.com>
2025-12-06 09:51:33 +0300

    29.2 Внутри ArrayList

--- src/main/java/ru/list/ListUsage.java
+++ src/main/java/ru/list/ListUsage.java
@@ -8,12 +8,10 @@ public class ListUsage {
         result.add("one");
         result.add("two");
         result.add("three");
-        List<String> list = new ArrayList<>();
-        list.add("four");
-        list.add("five");
-        result.addAll(list);
+        result.replaceAll(String::toUpperCase);
         for (String string : result) {
             System.out.println("Текущий элемент: " + string);
         }
     }
 }
+
