15bf6f0f3bf6141a1c34a6e160ea1022485f630c
valkravchen <100293539+valkravchen@users.noreply.github.com>
2025-12-07 16:04:59 +0300

    29.4 Динамический список на массиве

---
+++ src/main/java/ru/collection/SimpleArrayList.java
@@ -0,0 +1,54 @@
+package ru.collection;
+
+
+import java.util.Iterator;
+
+public class SimpleArrayList<T> implements SimpleList<T> {
+    private T[] container;
+    private int size;
+    private int modCount;
+
+    public SimpleArrayList(int capacity) {
+        this.container = (T[]) new Object[capacity];
+    }
+
+    @Override
+    public void add(T value) {
+
+    }
+
+    @Override
+    public T set(int index, T newValue) {
+        return null;
+    }
+
+    @Override
+    public T remove(int index) {
+        return null;
+    }
+
+    @Override
+    public T get(int index) {
+        return null;
+    }
+
+    @Override
+    public int size() {
+        return 0;
+    }
+
+    @Override
+    public Iterator<T> iterator() {
+        return new Iterator<T>() {
+            @Override
+            public boolean hasNext() {
+                return false;
+            }
+
+            @Override
+            public T next() {
+                return null;
+            }
+        };
+    }
+}
