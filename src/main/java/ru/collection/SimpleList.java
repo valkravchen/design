934d83b142632442aee2fc177d93958e75781e4e
valkravchen <100293539+valkravchen@users.noreply.github.com>
2025-12-07 15:54:50 +0300

    29.4 Динамический список на массиве

--- src/main/java/ru/collection/SimpleList.java
+++ src/main/java/ru/collection/SimpleList.java
@@ -2,8 +2,12 @@ package ru.collection;
 
 public interface SimpleList<T> extends Iterable<T> {
     void add(T value);
+
     T set(int index, T newValue);
+
     T remove(int index);
+
     T get(int index);
+
     int size();
 }
