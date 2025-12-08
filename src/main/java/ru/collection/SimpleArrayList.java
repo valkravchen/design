e74fc6d86c5e56142e29f9caf536e6d4bcb0db39
Valk <kravchenkoval@mail.ru>
2025-12-08 11:33:55 +0300

    Динамический массив

--- src/main/java/ru/collection/SimpleArrayList.java
+++ src/main/java/ru/collection/SimpleArrayList.java
@@ -1,5 +1,6 @@
 package ru.collection;
 
+import java.util.Arrays;
 import java.util.Iterator;
 
 public class SimpleArrayList<T> implements SimpleList<T> {
@@ -49,5 +50,39 @@ public class SimpleArrayList<T> implements SimpleList<T> {
                 return null;
             }
         };
+    }
+
+    public static void main(String[] args) {
+        int size = 0;
+        String[] strings = new String[5];
+        strings[size++] = "One";
+        strings[size++] = "Two";
+        strings[size++] = "Three";
+        System.out.println("Длина массива: " + strings.length);
+        System.out.println(Arrays.toString(strings));
+        System.out.println("Количество реально добавленных элементов: " + size);
+        String[] newStrings = Arrays.copyOf(strings, strings.length * 2);
+        System.out.println("Длина нового массива: " + newStrings.length);
+        System.out.println(Arrays.toString(strings));
+        System.out.println(Arrays.toString(newStrings));
+        strings = newStrings;
+        System.out.println(Arrays.toString(strings));
+
+
+
+        String[] array = {"one", "two", "three", "four", "five"};
+        System.out.println(Arrays.toString(array));
+        // индекс по которому удаляем
+        int index = 2;
+        System.arraycopy(
+                array, // откуда копируем
+                index + 1, // начиная с какого индекса
+                array, // куда копируем
+                index, // начиная с какого индекса
+                array.length - index - 1 // сколько элементов копируем
+        );
+        // на последнее место ставим null, чтобы не было утечки памяти (если удаляем последний элемент)
+        array[array.length - 1] = null;
+        System.out.println(Arrays.toString(array));
     }
 }
