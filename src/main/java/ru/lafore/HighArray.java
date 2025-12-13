f765910413544c0c311b5c07c3ee26ccd41a186a
valkravchen <100293539+valkravchen@users.noreply.github.com>
2025-12-13 21:03:34 +0300

    Структуры данных и алгоритмы в Java. Программа HighArray.java

---
+++ src/main/java/ru/lafore/HighArray.java
@@ -0,0 +1,50 @@
+package ru.lafore;
+
+public class HighArray {
+    private long[] array;
+    private int countElements;
+
+    public HighArray(int max) {
+        this.array = new long[max];
+        this.countElements = 0;
+    }
+
+    public boolean find(long searchKey) {
+        int index;
+        for (index = 0; index < countElements; index++) {
+            if (array[index] == searchKey) {
+                break;
+            }
+        }
+        return index != countElements;
+    }
+
+    public void insert(Long value) {
+        array[countElements++] = value;
+    }
+
+    public boolean delete(long value) {
+        int index;
+        for (index = 0; index < countElements; index++) {
+            if (value == array[index]) {
+                break;
+            }
+        }
+        if (index == countElements) {
+            return false;
+        } else {
+            for (int i = index; i < countElements; i++) {
+                array[i] = array[i + 1];
+            }
+            countElements--;
+            return true;
+        }
+    }
+
+    public void display() {
+        for (int i = 0; i < countElements; i++) {
+            System.out.print(array[i] + " ");
+            System.out.println();
+        }
+    }
+}
