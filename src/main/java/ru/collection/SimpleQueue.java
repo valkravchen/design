3bd1e3acddae4199a4690144d82cfe2e307d7f87
valkravchen <100293539+valkravchen@users.noreply.github.com>
2025-12-27 08:30:21 +0300

    29.8 Очередь в двух стеках

--- src/main/java/ru/collection/SimpleQueue.java
+++ src/main/java/ru/collection/SimpleQueue.java
@@ -2,6 +2,6 @@ package ru.collection;
 
 public class SimpleQueue<T> {
     private final SimpleStack<T> input = new SimpleStack<>();
-    private final  SimpleStack<T> output = new SimpleStack<>();
-    }
+    private final SimpleStack<T> output = new SimpleStack<>();
+}
 
