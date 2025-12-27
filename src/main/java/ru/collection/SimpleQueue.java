862dfe6441d01f0465f796adc0e36b3a7d870b17
valkravchen <100293539+valkravchen@users.noreply.github.com>
2025-12-27 09:06:49 +0300

    29.8 Очередь в двух стеках

--- src/main/java/ru/collection/SimpleQueue.java
+++ src/main/java/ru/collection/SimpleQueue.java
@@ -1,7 +1,32 @@
 package ru.collection;
 
+import java.util.NoSuchElementException;
+
 public class SimpleQueue<T> {
     private final SimpleStack<T> input = new SimpleStack<>();
     private final SimpleStack<T> output = new SimpleStack<>();
+    private int inputSize = 0;
+    private int outputSize = 0;
+
+    public T poll() {
+        if (inputSize == 0 && outputSize == 0) {
+            throw new NoSuchElementException("Queue is empty");
+        }
+        if (outputSize == 0) {
+            while (inputSize > 0) {
+                T element = input.pop();
+                output.push(element);
+                inputSize--;
+                outputSize++;
+            }
+        }
+        outputSize--;
+        return output.pop();
+    }
+
+    public void push(T value) {
+        input.push(value);
+        inputSize++;
+    }
 }
 
