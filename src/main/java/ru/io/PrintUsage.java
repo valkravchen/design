96e66cf95ec7c913305035779c87e6cdb0e11bc1
valkravchen <100293539+valkravchen@users.noreply.github.com>
2026-01-02 10:33:51 +0300

    33.5 Классы вывода PrintStream, PrintWriter

--- src/main/java/ru/io/PrintUsage.java
+++ src/main/java/ru/io/PrintUsage.java
@@ -1,4 +1,14 @@
 package ru.io;
 
+import java.io.IOException;
+import java.io.PrintStream;
+
 public class PrintUsage {
+    public static void main(String[] args) {
+        try (PrintStream stream = new PrintStream("data/print.txt")) {
+            stream.println("Из PrintStream в FileOutputStream");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
